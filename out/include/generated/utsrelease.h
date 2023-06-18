0c4ee76e0ef48dadf0eb633876a93ea867e23bcf
kodhh <limw94@outlook.com>
2023-06-18 08:48:35 +0800

    update .gitignore

--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.318-g8d6d5c09aaf7-dirty"
--- out/include/generated/vdso32-offsets.h
+++
@@ -1,4 +0,0 @@
-#define vdso_offset_compat_rt_sigreturn_arm	0x0be4
-#define vdso_offset_compat_rt_sigreturn_thumb	0x0bf8
-#define vdso_offset_compat_sigreturn_arm	0x0bd8
-#define vdso_offset_compat_sigreturn_thumb	0x0bf0
